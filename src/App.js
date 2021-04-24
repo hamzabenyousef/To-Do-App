8b3199804acb87598b922f41c5a35cff40f1798c
hamzabenyousef <hamza.bys@123gmail.com>
2021-04-24 18:16:58 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <img src="./hamzabys.jpg" className="App-logo" alt="logo" />
+     
+    <p> Ye sirine ye lokza rani n7ebek </p>
+    <p> T3ares beya ... ? </p>
+    
+    <button>Yes</button>
+    <br/>
+    <button>No</button>
+    </header>
     </div>
   );
 }
